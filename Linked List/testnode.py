583003d27cfd3cfd740894dfba20ce4be8e4f493
Jafet24k <joss_1983@outlook.com>
2022-11-13 18:10:41 -0600

    linked list added in a new directory and practice with nodes

---
+++ Linked List/testnode.py
@@ -0,0 +1,17 @@
+from node import Node
+
+node1 = None
+node2 = Node("A", None)
+node3 = Node("B", node2)
+node4 = Node("C", node1)
+
+# print(node3.next.data)
+
+head = None
+for count in range(1, 15):
+    head = Node(count, head)
+
+
+while head != None:
+    print(head.data)
+    head = head.next
